c67a168f2b888f75618d6c51fd8cf15e55f06c9e
Danielsio <kingddd301@gmail.com>
2022-09-02 20:16:35 +0300

    added useReducer

--- src/components/users/UserResaults.jsx
+++ src/components/users/UserResaults.jsx
@@ -1,25 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import GithubContext from "../../context/github/GithubContext";
 import Spinner from "../layout/Spinner";
 import UserItem from "./UserItem";
 
 function UserResaults() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const { users, loading, fetchUsers } = useContext(GithubContext);
 
   useEffect(() => {
     fetchUsers();
   }, []);
-
-  const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    });
-    const data = await response.json();
-    setUsers(data);
-    setLoading(false);
-  };
 
   if (!loading) {
     return (
